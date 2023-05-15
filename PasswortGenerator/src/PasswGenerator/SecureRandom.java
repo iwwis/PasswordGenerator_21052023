b978fa62c4c4f7b8a1dea7a258a0f16622736c78
iw <121799895+iwwis@users.noreply.github.com>
2023-05-15 11:12:44 +0200

    Aktualisierung 11:12

--- PasswortGenerator/src/PasswGenerator/SecureRandom.java
+++ PasswortGenerator/src/PasswGenerator/SecureRandom.java
@@ -6,12 +6,24 @@ package PasswGenerator;
 
 public class SecureRandom {
 	
+	// SecureRandom sollte deklariert werden als:
 	// SecureRandom random = new SecureRandom();
-    byte bytes[] = new byte[16];
-    // random.nextBytes(bytes);
+	// die Klasse SecureRandom wurde schon im PasswordGenerator deklariert
+	// als
+	// private static final SecureRandom secureRandom = new SecureRandom();
+
+	// die Variable bytes vom Typ byte wurde deklariert
+	// der Variable bytes wurde der Wert 16 zugewiesen
+	byte bytes[] = new byte[16];
     
+	// diese Zeile wurde bei der Oracle Doku vorgeschlagen, aber hier ergibt sie einen Fehler
+	// random.nextBytes(bytes);
+    
+	// diese Zeile wurde bei der Oracle Doku vorgeschlagen, aber hier ergibt sie einen Fehler
+	// seed bedeutet, dass man aus einem bestimmten Korn(seed) die Werte zum Generieren nimmt/get
     // byte seed[] = random.generateSeed(20);
     
+	
 	public int nextInt(int length) {
 		
 		return 0;
